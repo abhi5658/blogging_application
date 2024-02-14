2b2b9a4c292affe302c279a49abcf9b8e07fe4d8
Abhishek Shah <abhishekshah5658@gmail.com>
2024-02-14 23:44:08 +0530

    add live backend api base url for hosted frontend

--- frontend/src/service/blogService.js
+++ frontend/src/service/blogService.js
@@ -1,5 +1,5 @@
 // import blogBlogs from '../constants/blogs'
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001';
 
 // Function to make GET requests
 export const getBlogList = async () => {
