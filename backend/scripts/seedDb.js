43b915a7af3f34074e704a09a5b10a32cdf76e61
Abhishek Shah <abhishekshah5658@gmail.com>
2024-02-15 23:32:47 +0530

    inital postgres setup WIP

--- backend/scripts/seedDb.js
+++ backend/scripts/seedDb.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const { sequelize, Blog } = require('../src/models');
 const uuidV4 = require('uuid').v4;
 
--- backend/src/models.js
+++ backend/src/models.js
@@ -1,10 +1,26 @@
 const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: process.env.NODE_ENV === 'test' ? './database-test.sqlite3' : './database.sqlite3',
-  logging: process.env.NODE_ENV === 'test' ? false : undefined,
-});
+const POSTGRES_DATABASE_AVAILABLE = process.env.POSTGRES_DATABASE_AVAILABLE || false;
+let sequelize;
+if (POSTGRES_DATABASE_AVAILABLE) {
+  const {
+    POSTGRES_DATABASE_HOST: host,
+    POSTGRES_DATABASE_USERNAME: username,
+    POSTGRES_DATABASE_PASSWORD: password,
+    POSTGRES_DATABASE_NAME: dbname,
+  } = process.env
+  // sequelize = new Sequelize(dbname, username, password, {
+  //   host: host,
+  //   dialect: 'postgres',
+  //   ssl: { rejectUnauthorized: false }
+  // });
+  sequelize = new Sequelize('DATABASE_URI?ssl=true');
+} else {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: process.env.NODE_ENV === 'test' ? './database-test.sqlite3' : './database.sqlite3',
+    logging: process.env.NODE_ENV === 'test' ? false : undefined,
+  });
+}
 
 class Blog extends Sequelize.Model { }
 Blog.init(
